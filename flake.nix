2b687c9f07dc61e68504721ee252edb12eee26a7
pandaman64 <kointosudesuyo@infoseek.jp>
2022-02-19 21:06:03 +0900

    Add Rust crate

--- flake.nix
+++ flake.nix
@@ -1,8 +1,29 @@
 {
   description = "Nix Debugging Envionment";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/release-21.11;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.crate2nix = {
+    url = github:kolloch/crate2nix;
+    flake = false;
   };
+
+  outputs = { self, nixpkgs, flake-utils, crate2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        generatedCargoNix = (import "${crate2nix}/tools.nix" { inherit pkgs; }).generatedCargoNix;
+      in
+      rec {
+        packages = {
+          nde-core = (import (generatedCargoNix {
+            name = "nde-core";
+            src = ./nde-core;
+          }) {
+            inherit pkgs;
+          }).rootCrate.build;
+        };
+        defaultPackage = packages.nde-core;
+      }
+    );
 }
